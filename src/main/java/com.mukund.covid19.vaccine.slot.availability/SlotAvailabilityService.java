0f4a8215b8db3589992a02cb095b7781c2c885df
mukundpatel <mukundp.786@gmail.com>
2021-05-20 11:32:56 +0530

    notifier aws

--- src/main/java/com.mukund.covid19.vaccine.slot.availability/SlotAvailabilityService.java
+++ src/main/java/com.mukund.covid19.vaccine.slot.availability/SlotAvailabilityService.java
@@ -1,4 +1,25 @@
 package com.mukund.covid19.vaccine.slot.availability;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class SlotAvailabilityService {
+    public void runDisplay() {
+
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+        try {
+            engine.eval(new FileReader("/index.js"));
+            Invocable invocable = (Invocable) engine;
+            Object result;
+            result = invocable.invokeFunction("execute", null);
+            System.out.println(result);
+            System.out.println(result.getClass());
+        } catch (NoSuchMethodException | ScriptException | FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
